d40849739694e7cf515c534c122235540eec39d7
jlayher <john.e.layher@gmail.com>
2021-03-07 23:46:45 -0500

    Created Route Files and added Middleware Functions

---
+++ routes/course.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+
+const router = express.Router();
+
+// GET all courses
+
+
+// GET specific course
+
+
+// POST (create) new course
+
+
+// PUT (update) a course
+
+
+// DELETE a course
